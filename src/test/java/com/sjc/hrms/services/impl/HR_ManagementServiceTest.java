8ea75310925b31dfbd8a386e90392aa83a9daba7
robinraj <louisrobinraj@gmail.com>
2016-12-03 09:09:10 +0530

    added code for test

---
+++ src/test/java/com/sjc/hrms/services/impl/HR_ManagementServiceTest.java
@@ -0,0 +1,13 @@
+package com.sjc.hrms.services.impl;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class HR_ManagementServiceTest extends HrmsBaseTest {
+
+	@Test
+	public void testSample(){
+		assertNotNull("hi","hii");
+	}
+}
