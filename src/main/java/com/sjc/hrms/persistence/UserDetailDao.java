db418e5ecea364e4f6c297ecb873c5d56781df4b
louis <louis@louis-PC>
2016-12-07 21:56:11 +0530

    test

--- src/main/java/com/sjc/hrms/persistence/UserDetailDao.java
+++ src/main/java/com/sjc/hrms/persistence/UserDetailDao.java
@@ -1,14 +1,11 @@
 package com.sjc.hrms.persistence;
 
-import java.sql.SQLException;
-import java.util.List;
-
 import com.sjc.hrms.core.persistence.JpaDAO;
 import com.sjc.hrms.model.UserDetail;
 
 public interface UserDetailDao extends JpaDAO<UserDetail> {
 
-	UserDetail getUserDetails(UserDetail user) 
+	UserDetail getUserDetails(UserDetail user); 
 
 
 }
